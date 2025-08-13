0048c7a591dbf2d2eedd326dc753fb807bf04470
Aryag03 <97581608+Aryag03@users.noreply.github.com>
2025-08-13 18:08:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
-import streamlit as st
+import streamlit as st 
 
-st.title('🎈 App Name')
+st.write("To-Do List")
 
-st.write('Hello world!')
+task=st.text_input("Enter your Task", " ")
+
+if st.button("Add Task"):
+    if task:
+        st.session_state["task_list"].append(task)
+
+if "task_list" not in st.session_state:
+    st.session_state["task_list"] = []
+
+for i, t in enumerate(st.session_state["task_list"]):
+    st.write(f"{i+1}.{t}")   
+
+for i, t in enumerate(st.session_state["task_list"]):
+    if st.checkbox(f"{i+1}.t"): 
+        st.session_state["task_list"].remove(t)  
+
